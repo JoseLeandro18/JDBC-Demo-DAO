9d445d26cc21bd6eeec9d2baafa43ff66d739cfe
JoseLeandro <joseleand06@gmail.com>
2025-03-23 16:20:50 -0300

    DepartmentDao and SellerDao Interfaces

--- db/DbException.java
+++ db/DbException.java
@@ -1,8 +1,10 @@
 package db;
 
 public class DbException extends RuntimeException {
+    // SerialVersion
     private static final long serialVersionUID = 1L;
 
+    // Msg ERROR
     public DbException(String msg) {
         super(msg);
     }
--- db/DbIntegrityException.java
+++ db/DbIntegrityException.java
@@ -1,8 +1,10 @@
 package db;
 
 public class DbIntegrityException extends RuntimeException {
+    // SerialVersion
     private static final long serialVersionUID = 1L;
 
+    // Msg ERROR
     public DbIntegrityException(String msg) {
         super(msg);
     }
